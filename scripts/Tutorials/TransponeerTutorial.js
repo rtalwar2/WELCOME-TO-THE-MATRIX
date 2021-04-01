ea86f769a9da0b6bd6e9def42f9968b77d6a8ab9
Raman Talwar <raman.talwar@ugent.be>
2021-04-01 09:04:08 +0200

    "if" weggehaald uit refreshmethode in tutorials

--- scripts/Tutorials/TransponeerTutorial.js
+++ scripts/Tutorials/TransponeerTutorial.js
@@ -8,11 +8,13 @@ export class TransponeerTutorial extends Tutorial {
     kolom2 = 0;
     midden = 0;
     som = 0;
-    rij=0;kolom=0;
+    rij = 0;
+    kolom = -1;
+
     constructor(m1) {
         super(m1);
         this.stapnummer = 0;
-        this.data = new Matrix(m1.aantalKolommen, m1.aantalRijen,"x");
+        this.data = new Matrix(m1.aantalKolommen, m1.aantalRijen, "x");
         this.matrices.push(this.m1);
         this.aantal_matrices = this.matrices.length;
     }
@@ -20,28 +22,21 @@ export class TransponeerTutorial extends Tutorial {
     refresh(tutorialPage) {//de logica van de Transponeertutorial
         //{finished:boolean,data:een matrix, tekst:"de best passende beschrijving bij de huidige bewerking"}
         document.querySelectorAll(".rood").forEach(value => value.classList.remove("rood"));//verwijjdert alle vorige rode elementen
-        if(this.stapnummer===0){
+        this.kolom++;
+        if (this.kolom === this.m1.aantalKolommen) {
+            this.kolom = 0;
+            this.rij++;
+        }
+        if (this.rij === this.m1.aantalRijen) {
+            this.finished = true;
+        } else {
             tutorialPage.tabel1.querySelector(`[data-id='id_${this.rij}-${this.kolom}']`).classList.add("rood");//maakt de juite elementen rood
-            setTimeout(()=>tutorialPage.tabel3.querySelector(`[data-id='id_${this.kolom}-${this.rij}']`).classList.add("rood"),1)
-            //die timeout is er zodat de klasse rood pas wordt toegevoegd nadat de matrix in de html is geladen
-            this.stapnummer++;
+            setTimeout(() => tutorialPage.tabel3.querySelector(`[data-id='id_${this.kolom}-${this.rij}']`).classList.add("rood"), 1);
+            //     //die timeout is er zodat de klasse rood pas wordt toegevoegd nadat de matrix in de html is geladen
+
         }
-        else{
-            this.kolom++;
-            if(this.kolom===this.m1.aantalKolommen){
-                this.kolom=0;
-                this.rij++;
-            }
-            if(this.rij===this.m1.aantalRijen){
-                this.finished=true;
-            }
-            else{
-                tutorialPage.tabel1.querySelector(`[data-id='id_${this.rij}-${this.kolom}']`).classList.add("rood");
-                setTimeout(()=>tutorialPage.tabel3.querySelector(`[data-id='id_${this.kolom}-${this.rij}']`).classList.add("rood"),1);
-            }
-        }
-        this.data.matrix[this.kolom][this.rij]=this.m1.matrix[this.rij][this.kolom];
-        return {finished:this.finished,data:this.data, tekst:`rij ${this.rij} wordt kolom ${this.rij}`}
+        this.data.matrix[this.kolom][this.rij] = this.m1.matrix[this.rij][this.kolom];
+        return {finished: this.finished, data: this.data, tekst: `rij ${this.rij} wordt kolom ${this.rij}`}
     }
 
 }
--- scripts/Tutorials/VermenigvuldigTutorial.js
+++ scripts/Tutorials/VermenigvuldigTutorial.js
@@ -21,36 +21,28 @@ export class VermenigvuldigTutorial extends Tutorial {
 
     refresh(tutorialPage) {//de logica van de vermenigvuldigtutorial
         document.querySelectorAll(".rood").forEach(value => value.classList.remove("rood"));//maakt de juite elementen rood
-        // if (this.stapnummer === 0) {//als het de eerste stap is, incrementeer nog niets, ik heb geprobeerd om deze if eruit te halen maar dat is niet gelukt
-        //     this.stapnummer++;
-        //     //return {element1:[this.rij1,this.midden],element2:[this.midden,this.kolom2],data:this.data, tekst:`${this.m1.matrix[this.rij1][this.midden]} + ${this.m2.matrix[this.rij1][this.midden]} = ?`}
-        //     tutorialPage.tabel1.querySelector(`[data-id='id_${this.rij1}-${this.midden}']`).classList.add("rood");
-        //     tutorialPage.tabel2.querySelector(`[data-id='id_${this.midden}-${this.kolom2}']`).classList.add("rood");
-        // }
-        //else{
-            this.midden++;
-            if (this.midden === this.m1.aantalKolommen) {
-                this.midden = 0;
-                this.data.matrix[this.rij1][this.kolom2] = this.som;
-                this.rij1++;
-                this.som = 0;
-            }
 
-            if (this.rij1 === this.m1.aantalRijen) {
-                this.rij1 = 0;
-                this.kolom2++;
-            }
+        this.midden++;
+        if (this.midden === this.m1.aantalKolommen) {
+            this.midden = 0;
+            this.data.matrix[this.rij1][this.kolom2] = this.som;
+            this.rij1++;
+            this.som = 0;
+        }
 
-            if (this.kolom2 === this.m2.aantalKolommen) {
-                this.finished = true;
-            } else {//als de tutorial niet gedaan is, kleur de juiste elementen
-                tutorialPage.tabel1.querySelector(`[data-id='id_${this.rij1}-${this.midden}']`).classList.add("rood");
-                tutorialPage.tabel2.querySelector(`[data-id='id_${this.midden}-${this.kolom2}']`).classList.add("rood");
-          //  }
+        if (this.rij1 === this.m1.aantalRijen) {
+            this.rij1 = 0;
+            this.kolom2++;
+        }
+
+        if (this.kolom2 === this.m2.aantalKolommen) {
+            this.finished = true;
+        } else {//als de tutorial niet gedaan is, kleur de juiste elementen
+            tutorialPage.tabel1.querySelector(`[data-id='id_${this.rij1}-${this.midden}']`).classList.add("rood");
+            tutorialPage.tabel2.querySelector(`[data-id='id_${this.midden}-${this.kolom2}']`).classList.add("rood");
         }
 
         this.som += this.m1.matrix[this.rij1][this.midden] * this.m2.matrix[this.midden][this.kolom2];
-        //return {element1:[this.rij1,this.midden],element2:[this.midden,this.kolom2],finished:this.finished,data:this.data, tekst:`${this.m1.matrix[this.rij1][this.midden]} + ${this.m2.matrix[this.midden][this.kolom2]} = ?`}
         return {
             finished: this.finished,
             data: this.data,
