b0aba1b9208e059771e6483e052fd5288104b144
Raman Talwar <raman.talwar@ugent.be>
2021-04-09 11:57:21 +0200

    display van vermenigvuldig oefening afwerken + functionaliteit toevoegen en bug oplossen

--- scripts/Oefeningen/VermenigvuldigOefening.js
+++ scripts/Oefeningen/VermenigvuldigOefening.js
@@ -13,23 +13,26 @@ export class VermenigvuldigOefening extends Oefeningen {
         //this.data = new Matrix(m1.aantalRijen, m2.aantalKolommen, "0");
         this.matrices.push(this.matrix1);
         this.matrices.push(this.matrix2);
-        console.log(m1);
-        console.log(m2);
+    //    console.log(m1);
+      //  console.log(m2);
         this.oplossing= m1.vermenigvuldigMatrix(m2);
         this.aantal_matrices = this.matrices.length;
+        // console.log(this.oplossing)
     }
 
 
     getMatrix() {
         var rij = 0;
         let kolom=0;
-        let output=new Matrix(this.oplossing.length,this.oplossing[0].length);
+        let output=new Matrix(document.querySelector("#rijen").value,document.querySelector("#kolommen").value);
+        //console.log(output)
         document.querySelectorAll(".matrix_cell").forEach(  value => {
-            if (kolom===this.oplossing[0].length){
+            //console.log(`kolom= ${kolom}`)
+            if (kolom==document.querySelector("#kolommen").value){//moet met == ipv === anders werkt het niet
                 rij++;
                 kolom=0;
             }
-            console.log(`${rij} ${kolom}`)
+           // console.log(`${rij} ${kolom}`)
             output.matrix[rij][kolom]=parseInt(value.value);
             kolom++;
         })
@@ -38,17 +41,25 @@ export class VermenigvuldigOefening extends Oefeningen {
 
 
     correct(invul) {
-        let bool = true;
+        // console.log(this.oplossing.length);
+        // console.log(document.querySelector("#rijen").value)
+        // console.log(this.oplossing[0].length)
+        // console.log(document.querySelector("#kolommen").value)
+        if((this.oplossing.length!=document.querySelector("#rijen").value)&&(this.oplossing[0].length!=document.querySelector("#kolommen").value)){ //moet met != ipv !==
+            // console.log("ik geraak eerste tot hier!")
+            return false;
+        }
+        // console.log("ik geraak tot hier!")
+
         for (let i = 0; i < this.oplossing.length; i++) {
             for (let j = 0; j < this.oplossing[0].length; j++) {
                 console.log(` check if ${this.oplossing[i][j]} == ${invul[i][j]}`)
                 if (this.oplossing[i][j] !== invul[i][j]) {
-                    bool= false;
-                    return bool;
+                    return false;
                 }
             }
         }
-        return bool;
+        return true;
     }
 
     checkOplossing(object) {
@@ -62,10 +73,11 @@ export class VermenigvuldigOefening extends Oefeningen {
         }
     }
 
-    maakInvul() {
-        var rows = 3;
-        var columns = 3;
+    changeInvul(){
+        var rows = document.querySelector("#rijen").value;
+        var columns = document.querySelector("#kolommen").value;
         var form = document.getElementById("frm");
+
         // for (let i = 0; i < rows; i++) {
         //     for (let j = 0; j < columns; j++) {
         //         let input=document.createElement("input");
@@ -76,14 +88,14 @@ export class VermenigvuldigOefening extends Oefeningen {
         // }
 
         let tabel = document.createElement("table");
-        tabel.classList.add("u-center")
-        form.parentElement.classname="";
-        form.parentElement.classList.add(`col-md-${this.oplossing[0].length}`);
+        tabel.classList.add("u-center");
+        tabel.id="input_tabel";
+        form.parentElement.classList.remove(...form.parentElement.classList);
+        form.parentElement.classList.add(`col-md-${columns}`);
         tabel.innerText = "";
-        for (let i=0;i<this.oplossing.length;i++) {
+        for (let i=0;i<rows;i++) {
             let tr = document.createElement("tr");
-            for (let j=0;j<this.oplossing[0].length;j++) {
-
+            for (let j=0;j<columns;j++) {
                 let td = document.createElement("td");
                 let input=document.createElement("input");
                 input.classList.add("matrix_cell");
@@ -93,8 +105,61 @@ export class VermenigvuldigOefening extends Oefeningen {
             }
             tabel.appendChild(tr);
         }
-        form.appendChild(tabel);
+     //   console.log(document.querySelector("#input_tabel"))
+        if(document.querySelector("#input_tabel")!=null){
+            form.removeChild(document.querySelector("#input_tabel"));
 
+        }
+        form.appendChild(tabel);
+    }
+
+    maakInvul() {
+`        <div class="row">
+            <label for="rijen">aantal rijen:</label><input class="left" type="number" id="rijen" min="1" max="5">
+
+        </div>
+        <div class="row">
+            <label for="kolommen">aantal kolommen:</label><input class="left" type="number" id="kolommen" min="1" max="5">
+
+        </div>`
+    var form = document.getElementById("frm");
+        let row1=document.createElement("div");
+        row1.classList.add("row");
+        let input1=document.createElement("input");
+        input1.type="number";
+        input1.classList.add("left");
+        input1.id="rijen";
+        input1.min="1";
+        input1.max="5";
+        input1.value=5;
+        let label1=document.createElement("label");
+
+        label1.setAttribute("for","rijen");
+        label1.innerText="aantal rijen:";
+
+        let row2=document.createElement("div");
+        row2.classList.add("row");
+        let input2=document.createElement("input");
+        input2.type="number";
+        input2.classList.add("left");
+        input2.id="kolommen";
+        input2.value=2;
+        input2.min="1";
+        input2.max="5";
+        let label2=document.createElement("label");
+
+        label2.setAttribute("for","kolommen");
+        label2.innerText="aantal kolommen:";
+        row1.appendChild(label1);
+        row1.appendChild(input1)
+        row2.appendChild(label2);
+        row2.appendChild(input2)
+        form.appendChild(row1);
+        form.appendChild(row2);
+        input1.addEventListener("change",this.changeInvul);
+        input2.addEventListener("change",this.changeInvul);
+
+    this.changeInvul();
 
         `<div class="col-md-2">
                     <table class="table">
