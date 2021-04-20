103fce454a6099758dc971e96a0bbb1addd32ab0
oclemens <Othello.Clemens@UGent.be>
2021-04-20 09:36:44 +0200

    Aanpassingen uitleg

--- scripts/Tutorials/VermenigvuldigTutorial.js
+++ scripts/Tutorials/VermenigvuldigTutorial.js
@@ -77,8 +77,8 @@ export class VermenigvuldigTutorial extends Tutorial {
         return {
             finished: this.finished,
             data: {mat:this.data,hoofding:"product"},
-            tekst: this.uitleg + `<br> ${this.matrix1.matrix[this.rij1][this.kolom1]} * ${this.matrix2.matrix[this.kolom1][this.kolom2]} = ? --> ${this.matrix1.matrix[this.rij1][this.kolom1] * this.matrix2.matrix[this.kolom1][this.kolom2]} <br>`
-                + `<br>som = ${this.tekst} = (${this.som})`
+            tekst: this.uitleg + `<br> <h3>${this.matrix1.matrix[this.rij1][this.kolom1]} * ${this.matrix2.matrix[this.kolom1][this.kolom2]} ==> ${this.matrix1.matrix[this.rij1][this.kolom1] * this.matrix2.matrix[this.kolom1][this.kolom2]}</h3> <br>`
+                + `<br> <h3>Som = ${this.tekst} = (${this.som})</h3>`
         }
     }
 }
